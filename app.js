96e3f8f5ceb1eca45abcd9287cb870c379f34579
JuliaMichaela <juliaperez@yandex.ru>
2024-11-08 23:38:55 +0300

    updated grades

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const grades =[];
 
-grades.push =Math.floor(Math.random()*100);
+grades[0] =Math.floor(Math.random()*100);
+grades[1] =Math.floor(Math.random()*100);
+grades[2] =Math.floor(Math.random()*100);
+grades[3]=Math.floor(Math.random()*100);
+grades[4] =Math.floor(Math.random()*100);
+grades[5] =Math.floor(Math.random()*100);
+grades[6]=Math.floor(Math.random()*100);
+grades[7] =Math.floor(Math.random()*100);
+grades[8] =Math.floor(Math.random()*100);
+grades[9] =Math.floor(Math.random()*100);
+grades[10] =Math.floor(Math.random()*100);
+grades[11]=Math.floor(Math.random()*100);
 console.log (grades)
+console.log (grades.length);
